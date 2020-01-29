76b8eb7a8c0eabab90e2102df861ceb6ed5ff279
samaonline <samaonline@live.com>
2020-01-28 23:43:00 -0800

    imagnet

---
+++ imagenet/utils.py
@@ -0,0 +1,330 @@
+""" helper function
+
+author baiyu
+"""
+
+import sys
+
+import numpy as np
+
+import torch
+from torch.optim.lr_scheduler import _LRScheduler
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.data import sampler
+from torch.nn import functional as F
+from pdb import set_trace as st
+#from dataset import CIFAR100Train, CIFAR100Test
+
+def conv_orth_dist(kernel, stride = 1):
+    [o_c, i_c, w, h] = kernel.shape
+    assert (w == h),"Do not support rectangular kernel"
+    #half = np.floor(w/2)
+    assert stride<w,"Please use matrix orthgonality instead"
+    new_s = stride*(w-1) + w#np.int(2*(half+np.floor(half/stride))+1)
+    temp = torch.eye(new_s*new_s*i_c).reshape((new_s*new_s*i_c, i_c, new_s,new_s)).cuda()
+    out = (F.conv2d(temp, kernel, stride=stride)).reshape((new_s*new_s*i_c, -1))
+    Vmat = out[np.floor(new_s**2/2).astype(int)::new_s**2, :]
+    temp= np.zeros((i_c, i_c*new_s**2))
+    for i in range(temp.shape[0]):temp[i,np.floor(new_s**2/2).astype(int)+new_s**2*i]=1
+    return torch.norm( Vmat@torch.t(out) - torch.from_numpy(temp).float().cuda() )
+    
+def deconv_orth_dist(kernel, stride = 2):
+    [o_c, i_c, w, h] = kernel.shape
+    output = torch.conv2d(kernel, kernel, stride=stride, padding=2)
+    target = torch.zeros((o_c, o_c, output.shape[-2], output.shape[-2])).cuda()
+    target[:,:,1,1] = torch.eye(o_c).cuda()
+    return torch.norm( output - target )
+    
+def orth_dist(mat, stride=None):
+    mat = mat.reshape( (mat.shape[0], -1) )
+    if mat.shape[0] < mat.shape[1]:
+        mat = mat.permute(1,0)
+    return torch.norm( torch.t(mat)@mat - torch.eye(mat.shape[1]).cuda())
+
+def trace_batch(cov):
+    return torch.mean(torch.sum(torch.diagonal(cov, dim1=1, dim2=2), dim=1))
+
+def get_network(args, use_gpu=True, sigma=0.1):
+    """ return given network
+    """
+
+    if args.net == 'vgg16':
+        from models.vgg import vgg16_bn
+        net = vgg16_bn()
+    if args.net == 'vggpo16':
+        from models.vggpo import vgg16_bn
+        net = vgg16_bn(sigma = sigma)        
+    elif args.net == 'vgg13':
+        from models.vgg import vgg13_bn
+        net = vgg13_bn()
+    elif args.net == 'vgg11':
+        from models.vgg import vgg11_bn
+        net = vgg11_bn()
+    elif args.net == 'vgg19':
+        from models.vgg import vgg19_bn
+        net = vgg19_bn()
+    elif args.net == 'densenet121':
+        from models.densenet import densenet121
+        net = densenet121()
+    elif args.net == 'densenet161':
+        from models.densenet import densenet161
+        net = densenet161()
+    elif args.net == 'densenet169':
+        from models.densenet import densenet169
+        net = densenet169()
+    elif args.net == 'densenet201':
+        from models.densenet import densenet201
+        net = densenet201()
+    elif args.net == 'googlenet':
+        from models.googlenet import googlenet
+        net = googlenet()
+    elif args.net == 'inceptionv3':
+        from models.inceptionv3 import inceptionv3
+        net = inceptionv3()
+    elif args.net == 'inceptionv4':
+        from models.inceptionv4 import inceptionv4
+        net = inceptionv4()
+    elif args.net == 'inceptionresnetv2':
+        from models.inceptionv4 import inception_resnet_v2
+        net = inception_resnet_v2()
+    elif args.net == 'xception':
+        from models.xception import xception
+        net = xception()
+    elif args.net == 'resnet18':
+        from models.resnet import resnet18
+        net = resnet18()
+    elif args.net == 'resnetp18':
+        from models.resnetp import resnet18
+        net = resnet18(sigma=sigma) 
+    elif args.net == 'resnetpp18':
+        from models.resnetpp import resnet18
+        net = resnet18(sigma=sigma)
+    elif args.net == 'resnetpo18':
+        from models.resnetpo import resnet18
+        net = resnet18(sigma=sigma) 
+    elif args.net == 'resnetpo18vis':
+        from models.resnetpo_vis import resnet18
+        net = resnet18(sigma=sigma)         
+    elif args.net == 'resnet18vis':
+        from models.resnet_vis import resnet18
+        net = resnet18()
+    elif args.net == 'resnetpop18':
+        from models.resnetpop import resnet18
+        net = resnet18(sigma=sigma)                          
+    elif args.net == 'resnetpp218':
+        from models.resnetpp2 import resnet18
+        net = resnet18(sigma=sigma)
+    elif args.net == 'resnet34':
+        from models.resnet import resnet34
+        net = resnet34()
+    elif args.net == 'resnet50':
+        from models.resnet import resnet50
+        net = resnet50()
+    elif args.net == 'resnetpo50':
+        from models.resnetpo50 import resnet50
+        net = resnet50(sigma=sigma)       
+    elif args.net == 'resnetpo34':
+        from models.resnetpo50 import resnet34
+        net = resnet34(sigma=sigma)           
+    elif args.net == 'resnet101':
+        from models.resnet import resnet101
+        net = resnet101()
+    elif args.net == 'resnet152':
+        from models.resnet import resnet152
+        net = resnet152()
+    elif args.net == 'preactresnet18':
+        from models.preactresnet import preactresnet18
+        net = preactresnet18()
+    elif args.net == 'preactresnet34':
+        from models.preactresnet import preactresnet34
+        net = preactresnet34()
+    elif args.net == 'preactresnet50':
+        from models.preactresnet import preactresnet50
+        net = preactresnet50()
+    elif args.net == 'preactresnet101':
+        from models.preactresnet import preactresnet101
+        net = preactresnet101()
+    elif args.net == 'preactresnet152':
+        from models.preactresnet import preactresnet152
+        net = preactresnet152()
+    elif args.net == 'resnext50':
+        from models.resnext import resnext50
+        net = resnext50()
+    elif args.net == 'resnext101':
+        from models.resnext import resnext101
+        net = resnext101()
+    elif args.net == 'resnext152':
+        from models.resnext import resnext152
+        net = resnext152()
+    elif args.net == 'shufflenet':
+        from models.shufflenet import shufflenet
+        net = shufflenet()
+    elif args.net == 'shufflenetv2':
+        from models.shufflenetv2 import shufflenetv2
+        net = shufflenetv2()
+    elif args.net == 'squeezenet':
+        from models.squeezenet import squeezenet
+        net = squeezenet()
+    elif args.net == 'mobilenet':
+        from models.mobilenet import mobilenet
+        net = mobilenet()
+    elif args.net == 'mobilenetv2':
+        from models.mobilenetv2 import mobilenetv2
+        net = mobilenetv2()
+    elif args.net == 'nasnet':
+        from models.nasnet import nasnet
+        net = nasnet()
+    elif args.net == 'attention56':
+        from models.attention import attention56
+        net = attention56()
+    elif args.net == 'attention92':
+        from models.attention import attention92
+        net = attention92()
+    elif args.net == 'seresnet18':
+        from models.senet import seresnet18
+        net = seresnet18()
+    elif args.net == 'seresnet34':
+        from models.senet import seresnet34 
+        net = seresnet34()
+    elif args.net == 'seresnet50':
+        from models.senet import seresnet50 
+        net = seresnet50()
+    elif args.net == 'seresnet101':
+        from models.senet import seresnet101 
+        net = seresnet101()
+    elif args.net == 'seresnet152':
+        from models.senet import seresnet152
+        net = seresnet152()
+
+    else:
+        print('the network name you have entered is not supported yet')
+        sys.exit()
+    
+    if use_gpu:
+        net = net.cuda()
+
+    return net
+
+
+def get_training_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True, perc=1.0):
+    """ return training dataloader
+    Args:
+        mean: mean of cifar100 training dataset
+        std: std of cifar100 training dataset
+        path: path to cifar100 training python dataset
+        batch_size: dataloader batchsize
+        num_workers: dataloader num_works
+        shuffle: whether to shuffle 
+    Returns: train_data_loader:torch dataloader object
+    """
+
+    transform_train = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    #cifar100_training = CIFAR100Train(path, transform=transform_train)
+    
+    cifar100_training = torchvision.datasets.CIFAR100(root='./data', train=True, download=True, transform=transform_train)
+    if perc < 1.0:
+        idx = np.load("misc/cifar100_"+str(perc)+".npy")
+        cifar100_training = torch.utils.data.Subset(cifar100_training, idx)
+    cifar100_training_loader = DataLoader(
+        cifar100_training, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar100_training_loader
+
+def get_training_dataloaderIMAGENET(mean, std, batch_size=16, num_workers=2, shuffle=True, perc=1.0):
+    """ return training dataloader
+    Args:
+        mean: mean of cifar100 training dataset
+        std: std of cifar100 training dataset
+        path: path to cifar100 training python dataset
+        batch_size: dataloader batchsize
+        num_workers: dataloader num_works
+        shuffle: whether to shuffle 
+    Returns: train_data_loader:torch dataloader object
+    """
+
+    transform_train = transforms.Compose([
+        #transforms.ToPILImage(),
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation(15),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    #cifar100_training = CIFAR100Train(path, transform=transform_train)
+    
+    cifar100_training = torchvision.datasets.ImageNet(root='/home/peterwg/dataset/ILSVRC2012', split='train', download=False, transform=transform_train)
+    st()
+    if perc < 1.0:
+        idx = np.load("misc/cifar100_"+str(perc)+".npy")
+        cifar100_training = torch.utils.data.Subset(cifar100_training, idx)
+    cifar100_training_loader = DataLoader(
+        cifar100_training, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar100_training_loader
+
+def get_test_dataloader(mean, std, batch_size=16, num_workers=2, shuffle=True):
+    """ return training dataloader
+    Args:
+        mean: mean of cifar100 test dataset
+        std: std of cifar100 test dataset
+        path: path to cifar100 test python dataset
+        batch_size: dataloader batchsize
+        num_workers: dataloader num_works
+        shuffle: whether to shuffle 
+    Returns: cifar100_test_loader:torch dataloader object
+    """
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std)
+    ])
+    #cifar100_test = CIFAR100Test(path, transform=transform_test)
+    cifar100_test = torchvision.datasets.CIFAR100(root='./data', train=False, download=True, transform=transform_test)
+    cifar100_test_loader = DataLoader(
+        cifar100_test, shuffle=shuffle, num_workers=num_workers, batch_size=batch_size)
+
+    return cifar100_test_loader
+
+def compute_mean_std(cifar100_dataset):
+    """compute the mean and std of cifar100 dataset
+    Args:
+        cifar100_training_dataset or cifar100_test_dataset
+        witch derived from class torch.utils.data
+    
+    Returns:
+        a tuple contains mean, std value of entire dataset
+    """
+
+    data_r = np.dstack([cifar100_dataset[i][1][:, :, 0] for i in range(len(cifar100_dataset))])
+    data_g = np.dstack([cifar100_dataset[i][1][:, :, 1] for i in range(len(cifar100_dataset))])
+    data_b = np.dstack([cifar100_dataset[i][1][:, :, 2] for i in range(len(cifar100_dataset))])
+    mean = np.mean(data_r), np.mean(data_g), np.mean(data_b)
+    std = np.std(data_r), np.std(data_g), np.std(data_b)
+
+    return mean, std
+
+class WarmUpLR(_LRScheduler):
+    """warmup_training learning rate scheduler
+    Args:
+        optimizer: optimzier(e.g. SGD)
+        total_iters: totoal_iters of warmup phase
+    """
+    def __init__(self, optimizer, total_iters, last_epoch=-1):
+        
+        self.total_iters = total_iters
+        super().__init__(optimizer, last_epoch)
+
+    def get_lr(self):
+        """we will use the first m batches, and set the learning
+        rate to base_lr * m / total_iters
+        """
+        return [base_lr * self.last_epoch / (self.total_iters + 1e-8) for base_lr in self.base_lrs]
